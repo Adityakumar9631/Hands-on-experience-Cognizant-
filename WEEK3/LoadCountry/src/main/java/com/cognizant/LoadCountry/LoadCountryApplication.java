054f7db0b972c6920b9abacc044814716be09543
Aditya kumar <168696099+Adityakumar9631@users.noreply.github.com>
2026-07-25 20:55:15 +0530

    week-3(REST)

---
+++ WEEK3/LoadCountry/src/main/java/com/cognizant/LoadCountry/LoadCountryApplication.java
@@ -0,0 +1,36 @@
+package com.cognizant.LoadCountry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class LoadCountryApplication {
+
+	private static final Logger LOGGER =
+			LoggerFactory.getLogger(LoadCountryApplication.class);
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LoadCountryApplication.class, args);
+
+		displayCountry();
+	}
+
+	public static void displayCountry() {
+
+		ClassPathXmlApplicationContext context =
+				new ClassPathXmlApplicationContext("country.xml");
+
+		Country country = context.getBean("country", Country.class);
+
+		LOGGER.debug("Country Code : {}", country.getCode());
+		LOGGER.debug("Country Name : {}", country.getName());
+
+		LOGGER.debug("Country : {}", country);
+
+		context.close();
+	}
+}
---
+++ WEEK3/LoadCountry/src/test/java/com/cognizant/LoadCountry/LoadCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.LoadCountry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadCountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

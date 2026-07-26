63f5bd22c95bd18d8b8d578f18ec4cdabed4b481
Aditya kumar <168696099+Adityakumar9631@users.noreply.github.com>
2026-07-26 17:00:18 +0530

    Microservices

---
+++ Week4/account/src/main/java/com/cognizent/account/controller/AccountController.java
@@ -0,0 +1,13 @@
+package com.cognizent.account.controller;
+import com.cognizent.account.model.Account;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+    @GetMapping("/accounts/{number}")
+    public Account getAccount(@PathVariable String number){
+        return new Account(number,"Savings",234343);
+    }
+}
